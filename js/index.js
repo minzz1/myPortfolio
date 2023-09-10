f6fa7b73b6937860c1230da739802948d0f88890
minzz1 <129828826+minzz1@users.noreply.github.com>
2023-09-10 17:06:32 +0900

    -

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ window.onscroll = scrollIF();
 
 function scrollIF() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("gnb").style.top = "0px";
+    document.getElementById("gnb").style.top = "0";
   } else {
-    document.getElementById("gnb").style.top = "-80px";
+    document.getElementById("gnb").style.top = "-30px";
   }
 }
